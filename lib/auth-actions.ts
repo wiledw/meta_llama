942fdfd704ee45a52160db53a4759ea0bae79ebc
William <william.edwardo@gmail.com>
2024-11-23 13:31:02 -0500

    Fix error

--- lib/auth-actions.ts
+++ lib/auth-actions.ts
@@ -70,7 +70,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL}`,
+      redirectTo: `${process.env.VERCEL_URL}/`,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
