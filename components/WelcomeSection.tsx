e70b6bcffd22753619eb94c9202af5304959a6a5
raphael-64 <raphlee2006@gmail.com>
2024-11-23 15:51:18 -0500

    changed welcome

--- components/WelcomeSection.tsx
+++ components/WelcomeSection.tsx
@@ -3,11 +3,12 @@ import Link from "next/link";
 export function WelcomeSection() {
   return (
     <section className="text-center mt-16">
-      <h1 className="text-4xl font-bold mb-4">Welcome to TravelApp!</h1>
+      <h1 className="text-4xl font-bold mb-4">Welcome to Localist!</h1>
       <p className="text-muted-foreground text-lg">
-        Plan your perfect journey with us
+        Plan local. Plan better.
+        {/*Plan your perfect journey with us */}
       </p>
-      <Link href="/map">
+      <Link href="/components/Map">
         <button
           style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}
         >
